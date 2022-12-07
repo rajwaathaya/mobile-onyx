d8ee099c3d718f8f24cff6a742a7a723e89f4672
rajwaathaya <thrrxxaa@gmail.com>
2022-12-07 11:30:27 +0700

    ta mobile

---
+++ lib/data/resep.dart
@@ -0,0 +1,30 @@
+class resep {
+  String name, htm, tutorial, image;
+
+  resep(
+      {required this.name,
+      required this.htm,
+      required this.tutorial,
+      required this.image});
+}
+
+List<resep> dataResep = [
+  resep(
+      name: 'Belgravia Kitchen',
+      htm: '640k',
+      tutorial:
+          'Open plan kitchen from our Brigitte range showing off how you can tailor your kitchen to meet your needs. Doors are onyx grey in a high gloss finish with handleless design. Full range of Neff appliances with a Quooker hot tap, Blanco Sink and a Caple slim wine cooler. Wall units set off with Sensio under unit lighting. The worktops are Quartz Forms 20mm in white and a side table has been constructed using the same Quartz Form material – stunning design feature making this customer’s kitchen the heart of their home.',
+      image: 'home-design.jpg'),
+  resep(
+      name: 'Belgravia Kitchen',
+      htm: '640k',
+      tutorial:
+          'Open plan kitchen from our Brigitte range showing off how you can tailor your kitchen to meet your needs. Doors are onyx grey in a high gloss finish with handleless design. Full range of Neff appliances with a Quooker hot tap, Blanco Sink and a Caple slim wine cooler. Wall units set off with Sensio under unit lighting. The worktops are Quartz Forms 20mm in white and a side table has been constructed using the same Quartz Form material – stunning design feature making this customer’s kitchen the heart of their home.',
+      image: 'home-design.jpg'),
+  resep(
+      name: 'Belgravia Kitchen',
+      htm: '640k',
+      tutorial:
+          'Open plan kitchen from our Brigitte range showing off how you can tailor your kitchen to meet your needs. Doors are onyx grey in a high gloss finish with handleless design. Full range of Neff appliances with a Quooker hot tap, Blanco Sink and a Caple slim wine cooler. Wall units set off with Sensio under unit lighting. The worktops are Quartz Forms 20mm in white and a side table has been constructed using the same Quartz Form material – stunning design feature making this customer’s kitchen the heart of their home.',
+      image: 'home-design.jpg')
+];
